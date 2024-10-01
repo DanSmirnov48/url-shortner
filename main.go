8d56050f39f6e1b55359678f50bb04dc9ecb58d7
Dan <daniel.smirnov17@gmail.com>
2024-10-01 20:21:25 +0100

    added html template and server it at '/' route

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// Handler for the root URL "/"
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Parse the template file
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Pass data to the template
+	data := struct {
+		Title string
+	}{
+		Title: "There",
+	}
+
+	// Render the template with the data
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Register the handler for the root URL "/"
+	http.HandleFunc("/", handler)
+
+	// Start the server
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
